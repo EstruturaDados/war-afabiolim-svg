2659098d35f52b06f9796815c6afd36d97c4f502
afabiolim-svg <afabiolim@gmail.com>
2025-11-03 19:14:57 +0000

    teste4

--- Projeto_war2.c
+++ Projeto_war2.c
@@ -9,3 +9,26 @@ struct Territorio {
     char cor[15];
     int tropas;
 };
+
+
+// cadastrar os territórios
+void cadastrarTerritorios(struct Territorio *territorios, int qtd) {
+    printf("=== Cadastro de Territórios ===\n\n");
+    for (int i = 0; i < qtd; i++) {
+        printf("Cadastro do Território %d:\n", i + 1);
+
+        printf("Nome: ");
+        fgets(territorios[i].nome, sizeof(territorios[i].nome), stdin);
+        territorios[i].nome[strcspn(territorios[i].nome, "\n")] = '\0';
+
+        printf("Cor do exército: ");
+        fgets(territorios[i].cor, sizeof(territorios[i].cor), stdin);
+        territorios[i].cor[strcspn(territorios[i].cor, "\n")] = '\0';
+
+        printf("Número de tropas: ");
+        scanf("%d", &territorios[i].tropas);
+        // limpa o buffer
+        getchar();
+        printf("\n");
+    }
+}
